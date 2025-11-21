0746c322f476a247a1022f9944cb738c84118971
assortednuts <korvin@coollink.org>
2025-11-21 01:49:37 -0600

    current state of me desktop before I mess it up

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+require "opts"
+require "keymaps"
+require "launch"
+
+spec("plugin.theme")
+spec("plugin.treesitter")
+spec("plugin.telescope")
+spec("plugin.mason")
+spec("plugin.lsp-zero")
+spec("plugin.nvimtree")
+spec("plugin.autopairs")
+spec("plugin.lspconfig")
+
+require "plugin.lazy"
